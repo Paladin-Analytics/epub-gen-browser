b739c15bd382cc18b403b8440e8cf652d524a2af
Maryam Yumna <maryam@surge.global>
2023-09-20 17:09:13 +0530

    AT-1879 fixed CORS error for exports with images

--- lib/util/fetchable-browser.ts
+++ lib/util/fetchable-browser.ts
@@ -4,6 +4,9 @@ export const type = 'blob';
 const fetchable = async (url: string, timeout: number) => {
   const controller = typeof AbortController !== "undefined" ? new AbortController() : {} as AbortController;
   const out = setTimeout(() => controller.abort && controller.abort(), timeout);
+  
+  //appending a query parameter to the image url to avoid the CORS error caused due to the browser cache issue
+  url = url+ "?export=epub";
 
   try {
     const res = await fetch(url, { signal: controller.signal });
